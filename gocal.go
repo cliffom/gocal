2c7056096192fe3e347b04d4e750b21d097f5f52
Michael Clifford <cliffom@gmail.com>
2020-02-26 12:00:33 -0800

    Added healthcheck

--- gocal.go
+++ gocal.go
@@ -9,6 +9,29 @@ import (
 	"github.com/cliffom/gocal/schema"
 )
 
+// Status is the struct used for healthchecks
+type Status struct {
+	Environment string `json:"environment"`
+	Status      string `json:"status"`
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	env := os.Getenv("ENVIRONMENT")
+	if env == "" {
+		env = "development"
+	}
+
+	status := &Status{
+		Environment: env,
+		Status:      "healthy",
+	}
+
+	data, _ := json.Marshal(status)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
 func createClassListing(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	c := schema.Class{}
@@ -34,6 +57,7 @@ func main() {
 	port := getPort()
 
 	log.Printf("Listening on 0.0.0.0:%s", port)
+	http.HandleFunc("/api/classes/_healthcheck", healthcheck)
 	http.HandleFunc("/api/classes/", createClassListing)
 	http.ListenAndServe(":"+port, nil)
 }
